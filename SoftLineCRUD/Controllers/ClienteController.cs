74cb44668a8619d3715300296a92d39c973d52dc
Jonathan <jon_sousapires@hotmail.com>
2026-02-26 20:59:09 -0300

    Adicionar arquivos de projeto.

---
+++ SoftLineCRUD/Controllers/ClienteController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoftLineCRUD.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AdicionarCliente()
+        {
+            return View();
+        }
+
+        public IActionResult EditarCliente()
+        {
+            return View();
+        }
+
+        public IActionResult VisualizarCliente()
+        {
+            return View();
+        }
+
+        public IActionResult ExcluirCliente()
+        {
+            return View();
+        }
+    }
+}
